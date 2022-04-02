fcc50362cb2d29da1d87412c8a7731fee3c52819
rmcsweeney <rmcsweeney@utexas.edu>
2022-04-02 00:08:35 -0500

    Initial Commit

---
+++ ManhuntEX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ManhuntEX
+//
+//  Created by Ryan McSweeney on 4/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ManhuntEX/ManhuntEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManhuntEXApp.swift
+//  ManhuntEX
+//
+//  Created by Ryan McSweeney on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ManhuntEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
